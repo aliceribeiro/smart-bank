59105a8e70266ef6fef9a2852a9f1b1780e812f2
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-21 22:29:58 -0300

    feat: :sparkles: cria e define os temas claro e escuro na aplicação

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -4,15 +4,15 @@ import outros from "../assets/images/outros.svg";
 import saude from "../assets/images/saude.svg";
 import transporte from "../assets/images/transporte.svg";
 import utilidades from "../assets/images/utilidades.svg";
-import { Icone } from "./UI";
+import { IconeTema } from "./UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-    Saude: <Icone src={saude} alt="Saúde" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    default: <Icone src={outros} alt="Outro" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    Saude: <IconeTema src={saude} alt="Saúde" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    default: <IconeTema src={outros} alt="Outro" />,
   };
 
   return Images[type] || Images.default;
